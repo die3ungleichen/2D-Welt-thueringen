a1c0e7770f070dd9258a8f3dd97db17d160b696a
Harfi <5414714-Harfi@users.noreply.gitlab.com>
2021-01-25 22:40:25 +0100

    3d-Druckegit status

---
+++ SPACE/3Dprinter.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="3Dprinter" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="tilesets/3Dprinter.png" width="96" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
